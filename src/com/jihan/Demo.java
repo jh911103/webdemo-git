0727e3df69ff2329a33c93a736eef67afd37280d
jihan <2533659732@qq.com>
2020-06-26 18:05:08 +0800

    项目经理

--- src/com/jihan/Demo.java
+++ src/com/jihan/Demo.java
@@ -2,7 +2,7 @@ package com.jihan;
 
 public class Demo {
     public static void main(String[] args){
-        System.out.println("我的第一个idea 程序员");
+        System.out.println("我的第一个idea 程序员 项目经理");
         //开发人员写了一句话
     }
 }
